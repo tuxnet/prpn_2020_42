b388e948c49ff580b4f44953801b44023946b220
Daniel Pidt <daniel.pidt@experteach.de>
2020-10-15 08:46:17 +0200

    fileio

---
+++ tag3/beispiele/SimpleFileIO.py
@@ -0,0 +1,22 @@
+# myfile = open('dirlist.txt')
+# print(type(myfile))
+# content = myfile.readlines()
+# print(type(content))
+# print(content)
+# for item in content:
+#     print(item)
+# myfile.close()
+# myfile = open('dirlist.txt')
+# print(myfile.readline())
+# print(myfile.readline())
+# myfile.close()
+# myfile = open('dirlist.txt')
+# outfile = open('newdirlist.txt', 'w') # ('r','w','a', 'x')
+# for line in myfile:
+#     print(line)
+#     outfile.write(line.upper())
+# myfile.close()
+# outfile.close()
+with open('dirlist.txt') as f:
+    print(f.readlines())
+print(f.closed)
